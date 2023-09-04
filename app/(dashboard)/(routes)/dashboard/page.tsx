e1eb520963866ee8242b17ea1a9ad9b75af83027
KranePch <teevisit_kn@hotmail.com>
2023-09-04 18:55:30 +0700

    Add chat page and working on APIs

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <p>
+        Dashboard Page
+      </p>
+    </div>
+  )
+}
